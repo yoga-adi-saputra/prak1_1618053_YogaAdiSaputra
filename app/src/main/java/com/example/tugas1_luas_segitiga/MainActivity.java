1c7f0e2451ed606f0dbf852f70713d2377d412a0
yoga-adi-saputra <visectedbdm01@gmail.com>
2021-10-04 16:24:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas1_luas_segitiga/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tugas1_luas_segitiga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText edit_alas = (EditText) findViewById(R.id.setalas);
+        final EditText edit_tinggi = (EditText) findViewById(R.id.settinggi);
+        final EditText txt_hasil = (EditText) findViewById(R.id.sethasil);
+        final Button hasil = (Button) findViewById(R.id.btn_hitung);
+
+        hasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double v1 = Double.parseDouble(edit_alas.getText().toString());
+                Double v2 = Double.parseDouble(edit_tinggi.getText().toString());
+                Double perhitungan = 0.5*v1*v2;
+                txt_hasil.setText(Double.toString(perhitungan));
+            }
+        });
+    }
+}
